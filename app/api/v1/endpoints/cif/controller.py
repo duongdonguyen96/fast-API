043af43704a4d211732140cdc4df20eb720dec9e
thinhnx <thinhnx@Minerva.vn>
2021-10-14 16:49:13 +0700

    [refactor](#26): rename variable

--- app/api/v1/endpoints/cif/controller.py
+++ app/api/v1/endpoints/cif/controller.py
@@ -4,7 +4,7 @@ from app.api.v1.endpoints.cif.repository import repos_get_cif_info
 
 class CtrCustomer(Controller):
     async def ctr_cif_info(self, customer_id: str):
-        is_found, customer_cif_info = await repos_get_cif_info(customer_id)
+        is_found, cif_info = await repos_get_cif_info(customer_id)
         if not is_found:
-            return self.response_exception(msg=customer_cif_info, loc="customer id")
-        return self.response(customer_cif_info)
+            return self.response_exception(msg=cif_info, loc="customer id")
+        return self.response(cif_info)
