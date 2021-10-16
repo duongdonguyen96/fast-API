47306dcd8916600864c0ca95d39520f0b2bca08a
Võ Văn Dương <duongvv@minerva.vn>
2021-10-16 14:29:47 +0700

    [docs]: change name tags

--- app/api/v1/endpoints/cif/basic_information/router.py
+++ app/api/v1/endpoints/cif/basic_information/router.py
@@ -8,5 +8,5 @@ router_step = APIRouter()
 # step I. Thông tin cá nhân -> 1. Giấy tờ định danh
 router_step.include_router(
     router=views_step_i_1.router_sub_step,
-    prefix="/identity", tags=['[CIF] I. TTCN']
+    prefix="/identity"
 )
