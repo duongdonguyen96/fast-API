8d8e7939cfca11420f3f74870a4abd724590b90e
Võ Văn Dương <duongvv@minerva.vn>
2021-10-18 16:26:20 +0700

    [add]: router step V. Debit card

--- app/api/v1/endpoints/cif/router.py
+++ app/api/v1/endpoints/cif/router.py
@@ -4,6 +4,7 @@ from app.api.v1.endpoints.cif import view as views_cif_info
 from app.api.v1.endpoints.cif.basic_information import router as routers_step_1
 from app.api.v1.endpoints.cif.basic_information.identity.identity_document import \
     view as views_step_i_1_a
+from app.api.v1.endpoints.cif.debit_card import view as views_debit_card
 from app.api.v1.endpoints.cif.e_banking import view as views_e_banking
 from app.api.v1.endpoints.cif.other_information import view as views_other_info
 from app.api.v1.endpoints.cif.payment_account import \
@@ -32,3 +33,7 @@ router_module.include_router(router=routers_payment_account.router_step, prefix=
 # step IV. E-banking
 router_module.include_router(router=views_e_banking.router, prefix="/{cif_id}/e-banking",
                              tags=['[CIF] IV. E-banking'])
+
+# step V. Thẻ ghi nợ
+router_module.include_router(router=views_debit_card.router, prefix="/{cif_id}/debit-card",
+                             tags=['[CIF] V. Thẻ ghi nợ'])
