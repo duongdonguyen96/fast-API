853a6c5f39a1d187e7f5270367671d0c93749a4c
Võ Văn Dương <duongvv@minerva.vn>
2021-10-18 16:15:37 +0700

    [refactor]: rename router

--- app/api/v1/endpoints/cif/router.py
+++ app/api/v1/endpoints/cif/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from app.api.v1.endpoints.cif import view as views_cif_info
-from app.api.v1.endpoints.cif.basic_information import router as views_step_1
+from app.api.v1.endpoints.cif.basic_information import router as routers_step_1
 from app.api.v1.endpoints.cif.basic_information.identity.identity_document import \
     view as views_step_i_1_a
 from app.api.v1.endpoints.cif.other_information import view as views_other_info
@@ -14,7 +14,7 @@ router_module = APIRouter()
 router_module.include_router(router=views_cif_info.router, tags=["[CIF] Information"])
 
 # step I. Thông tin cá nhân
-router_module.include_router(router=views_step_1.router_step, prefix="/{cif_id}/basic-information",
+router_module.include_router(router=routers_step_1.router_step, prefix="/{cif_id}/basic-information",
                              tags=['[CIF] I. TTCN'])
 
 # router đặc biệt, do không sử dụng prefix có path param là {cif_id}
--- app/api/v1/endpoints/router.py
+++ app/api/v1/endpoints/router.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints.cif import router as views_cif
-from app.api.v1.endpoints.config import router as views_config
+from app.api.v1.endpoints.cif import router as routers_cif
+from app.api.v1.endpoints.config import router as routers_config
 from app.api.v1.endpoints.user import view as views_user
 
 router = APIRouter()
 
 router.include_router(router=views_user.router, prefix="/users", tags=["User"])
 
-router.include_router(router=views_config.router, prefix="/config", tags=["Configs"])
+router.include_router(router=routers_config.router, prefix="/config", tags=["Configs"])
 
-router.include_router(router=views_cif.router_module, prefix="/cif")
+router.include_router(router=routers_cif.router_module, prefix="/cif")
