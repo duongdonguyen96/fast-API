4bc6b4fcfcdf5294e5e9570557a290f8880e9b4d
thinhnx <thinhnx@Minerva.vn>
2021-10-18 16:52:42 +0700

    [refactor](#57): delete example request, response

--- app/api/v1/endpoints/cif/other_information/schema.py
+++ app/api/v1/endpoints/cif/other_information/schema.py
@@ -1,6 +1,6 @@
 from pydantic import Field
 
-from app.api.base.schema import BaseSchema, CreatedUpdatedBaseModel
+from app.api.base.schema import BaseSchema
 from app.api.v1.schemas.utils import DropdownRequest, DropdownResponse
 
 
@@ -26,37 +26,3 @@ class OtherInformationUpdateRequest(BaseSchema):
     )
     sale_staff: DropdownRequest = Field(..., description="Mã nhân viên kinh doanh")
     indirect_sale_staff: DropdownRequest = Field(..., description="Mã nhân viên kinh doanh gián tiếp")
-
-
-class OtherInformationUpdateResponse(CreatedUpdatedBaseModel):
-    pass
-
-
-EXAMPLE_REQUEST_UPDATE_OTHER_INFO = {
-    "ex1": {
-        "summary": "Example update other information.",
-        "description": "Data update other information.",
-        "value": {
-            "legal_agreement_flag": True,
-            "advertising_marketing_flag": False,
-            "sale_staff": {"id": "123"},
-            "indirect_sale_staff": {"id": "123"}
-        }
-    }
-
-}
-
-EXAMPLE_RESPONSE_SUCCESS_UPDATE_OTHER_INFO = {
-    "ex1": {
-        "summary": "Success.",
-        "value": {
-            "data": {
-                "created_at": "2021/10/16 10:46:08",
-                "created_by": "system",
-                "updated_at": "2021/10/16 10:46:08",
-                "updated_by": "system",
-            },
-            "errors": []
-        }
-    }
-}
--- app/api/v1/endpoints/cif/other_information/view.py
+++ app/api/v1/endpoints/cif/other_information/view.py
@@ -1,13 +1,11 @@
-from fastapi import APIRouter, Body, Depends, Path
+from fastapi import APIRouter, Depends, Path
 from starlette import status
 
-from app.api.base.schema import ResponseData
+from app.api.base.schema import CreatedUpdatedBaseModel, ResponseData
 from app.api.v1.dependencies.authenticate import get_current_user_from_header
 from app.api.v1.endpoints.cif.other_information.controller import CtrOtherInfo
 from app.api.v1.endpoints.cif.other_information.schema import (
-    EXAMPLE_REQUEST_UPDATE_OTHER_INFO,
-    EXAMPLE_RESPONSE_SUCCESS_UPDATE_OTHER_INFO, OtherInformationResponse,
-    OtherInformationUpdateRequest, OtherInformationUpdateResponse
+    OtherInformationResponse, OtherInformationUpdateRequest
 )
 from app.utils.swagger import swagger_response
 
@@ -31,23 +29,19 @@ async def view_other_info(
     return ResponseData[OtherInformationResponse](**other_info)
 
 
-@router.put(
+@router.post(
     path="/",
     name="Update",
     description="Cập nhật dữ liệu tab `II. THÔNG TIN KHÁC` của khách hàng",
     responses=swagger_response(
-        response_model=ResponseData[OtherInformationUpdateResponse],
+        response_model=ResponseData[CreatedUpdatedBaseModel],
         success_status_code=status.HTTP_200_OK,
-        success_examples=EXAMPLE_RESPONSE_SUCCESS_UPDATE_OTHER_INFO
     ),
 )
 async def view_update_other_info(
+        update_other_info_req: OtherInformationUpdateRequest,
         cif_id: str = Path(..., description='Id CIF ảo'),
-        update_other_info_req: OtherInformationUpdateRequest = Body(
-            ...,
-            examples=EXAMPLE_REQUEST_UPDATE_OTHER_INFO,
-        ),
         current_user=Depends(get_current_user_from_header())
 ):
     update_other_info = await CtrOtherInfo(current_user).ctr_update_other_info(cif_id, update_other_info_req)
-    return ResponseData[OtherInformationUpdateResponse](**update_other_info)
+    return ResponseData[CreatedUpdatedBaseModel](**update_other_info)
