38d15e73ddc4e6d94515e7958b0314e2b7518508
thinhnx <thinhnx@Minerva.vn>
2021-10-18 16:26:27 +0700

    [refactor](#56)[add](#57): refactor name in #56, update other information in #57

--- app/api/v1/endpoints/cif/other_information/schema.py
+++ app/api/v1/endpoints/cif/other_information/schema.py
@@ -1,14 +1,62 @@
 from pydantic import Field
 
-from app.api.base.schema import BaseSchema
-from app.api.v1.schemas.utils import DropdownResponse
+from app.api.base.schema import BaseSchema, CreatedUpdatedBaseModel
+from app.api.v1.schemas.utils import DropdownRequest, DropdownResponse
 
 
 class OtherInformationResponse(BaseSchema):
     legal_agreement_flag: bool = Field(..., description="cờ thỏa thuận pháp lý `True`: có , `False`: không ")
-    advertising_marketing_flag: bool = Field(..., description="Cờ đồng ý nhận SMS, Email tiếp thị quảng cáo từ SCB. "
-                                                              "`True`: có, "
-                                                              "`False`: không."
-                                             )
+    advertising_marketing_flag: bool = Field(
+        ...,
+        description="Cờ đồng ý nhận SMS, Email tiếp thị quảng cáo từ SCB. "
+                    "`True`: có, "
+                    "`False`: không."
+    )
     sale_staff: DropdownResponse = Field(..., description="Mã nhân viên kinh doanh")
     indirect_sale_staff: DropdownResponse = Field(..., description="Mã nhân viên kinh doanh gián tiếp")
+
+
+class OtherInformationUpdateRequest(BaseSchema):
+    legal_agreement_flag: bool = Field(..., description="cờ thỏa thuận pháp lý `True`: có , `False`: không ")
+    advertising_marketing_flag: bool = Field(
+        ...,
+        description="Cờ đồng ý nhận SMS, Email tiếp thị quảng cáo từ SCB. "
+                    "`True`: có, "
+                    "`False`: không."
+    )
+    sale_staff: DropdownRequest = Field(..., description="Mã nhân viên kinh doanh")
+    indirect_sale_staff: DropdownRequest = Field(..., description="Mã nhân viên kinh doanh gián tiếp")
+
+
+class OtherInformationUpdateResponse(CreatedUpdatedBaseModel):
+    pass
+
+
+EXAMPLE_REQUEST_UPDATE_OTHER_INFO = {
+    "ex1": {
+        "summary": "Example update other information.",
+        "description": "Data update other information.",
+        "value": {
+            "legal_agreement_flag": True,
+            "advertising_marketing_flag": False,
+            "sale_staff": {"id": "123"},
+            "indirect_sale_staff": {"id": "123"}
+        }
+    }
+
+}
+
+EXAMPLE_RESPONSE_SUCCESS_UPDATE_OTHER_INFO = {
+    "ex1": {
+        "summary": "Success.",
+        "value": {
+            "data": {
+                "created_at": "2021/10/16 10:46:08",
+                "created_by": "system",
+                "updated_at": "2021/10/16 10:46:08",
+                "updated_by": "system",
+            },
+            "errors": []
+        }
+    }
+}
--- app/api/v1/endpoints/cif/other_information/view.py
+++ app/api/v1/endpoints/cif/other_information/view.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter, Depends, Path
+from fastapi import APIRouter, Body, Depends, Path
 from starlette import status
 
 from app.api.base.schema import ResponseData
 from app.api.v1.dependencies.authenticate import get_current_user_from_header
 from app.api.v1.endpoints.cif.other_information.controller import CtrOtherInfo
 from app.api.v1.endpoints.cif.other_information.schema import (
-    OtherInformationResponse
+    EXAMPLE_REQUEST_UPDATE_OTHER_INFO,
+    EXAMPLE_RESPONSE_SUCCESS_UPDATE_OTHER_INFO, OtherInformationResponse,
+    OtherInformationUpdateRequest, OtherInformationUpdateResponse
 )
 from app.utils.swagger import swagger_response
 
@@ -21,9 +23,31 @@ router = APIRouter()
         success_status_code=status.HTTP_200_OK
     )
 )
-async def view_cif_info(
+async def view_other_info(
         cif_id: str = Path(..., description='Id CIF ảo'),
         current_user=Depends(get_current_user_from_header())
 ):
     other_info = await CtrOtherInfo(current_user).ctr_other_info(cif_id)
     return ResponseData[OtherInformationResponse](**other_info)
+
+
+@router.put(
+    path="/",
+    name="Update",
+    description="Cập nhật dữ liệu tab `II. THÔNG TIN KHÁC` của khách hàng",
+    responses=swagger_response(
+        response_model=ResponseData[OtherInformationUpdateResponse],
+        success_status_code=status.HTTP_200_OK,
+        success_examples=EXAMPLE_RESPONSE_SUCCESS_UPDATE_OTHER_INFO
+    ),
+)
+async def view_update_other_info(
+        cif_id: str = Path(..., description='Id CIF ảo'),
+        update_other_info_req: OtherInformationUpdateRequest = Body(
+            ...,
+            examples=EXAMPLE_REQUEST_UPDATE_OTHER_INFO,
+        ),
+        current_user=Depends(get_current_user_from_header())
+):
+    update_other_info = await CtrOtherInfo(current_user).ctr_update_other_info(cif_id, update_other_info_req)
+    return ResponseData[OtherInformationUpdateResponse](**update_other_info)
