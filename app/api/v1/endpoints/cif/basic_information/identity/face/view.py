bbeace9f0990e54166f3497a98eb2c0e640cb0b2
cuonglv4 <cuonglv4@scb.com.vn>
2021-10-19 10:16:58 +0700

    [add](#36): add get data face

--- app/api/v1/endpoints/cif/basic_information/identity/face/view.py
+++ app/api/v1/endpoints/cif/basic_information/identity/face/view.py
@@ -1,9 +1,17 @@
+from typing import List
+
 from fastapi import APIRouter, Depends, Path
 from starlette import status
 
 from app.api.base.schema import ResponseData
 from app.api.base.swagger import swagger_response
 from app.api.v1.dependencies.authenticate import get_current_user_from_header
+from app.api.v1.endpoints.cif.basic_information.identity.face.controller import (
+    CtrFace
+)
+from app.api.v1.endpoints.cif.basic_information.identity.face.schema import (
+    FacesResponse
+)
 from app.api.v1.endpoints.user.schema import UserInfoResponse
 
 router = APIRouter()
@@ -26,15 +34,15 @@ async def view_create_identity_document(current_user=Depends(get_current_user_fr
 @router.get(
     path="/",
     name="1. GTĐD - B. Khuôn mặt",
-    description="Detail",
+    description="Lấy dữ liệu tab `KHUÔN MẶT` của khách hàng",
     responses=swagger_response(
-        response_model=ResponseData[UserInfoResponse],
+        response_model=ResponseData[List[FacesResponse]],
         success_status_code=status.HTTP_200_OK
     )
 )
-async def view_retrieve_user(
+async def view_retrieve_face(
         cif_id: str = Path(...),
         current_user=Depends(get_current_user_from_header())
 ):
-    data = {'cif_id': cif_id}
-    return ResponseData[UserInfoResponse](**data)
+    face_info = await CtrFace(current_user).ctr_get_list_face(cif_id)
+    return ResponseData[List[FacesResponse]](**face_info)
