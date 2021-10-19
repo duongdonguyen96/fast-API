c0c0f94d7f8746e9abb191c0312e3f4cf7257eb0
Võ Văn Dương <duongvv@minerva.vn>
2021-10-19 12:00:18 +0700

    [refactor]: split to two files

--- app/api/v1/schemas/cif.py
+++ app/api/v1/schemas/cif.py
@@ -4,6 +4,20 @@ from app.api.base.schema import BaseSchema
 from app.api.v1.schemas.utils import DropdownRequest, DropdownResponse
 
 
+class AddressResponse(BaseSchema):
+    province: DropdownResponse = Field(..., description="Tỉnh/Thành phố")
+    district: DropdownResponse = Field(..., description="Quận/Huyện")
+    ward: DropdownResponse = Field(..., description="Phường/Xã")
+    number_and_street: str = Field(..., description="Số nhà, tên đường")
+
+
+class AddressRequest(BaseSchema):
+    province: DropdownResponse = Field(..., description="Tỉnh/Thành phố")
+    district: DropdownResponse = Field(..., description="Quận/Huyện")
+    ward: DropdownResponse = Field(..., description="Phường/Xã")
+    number_and_street: str = Field(..., description="Số nhà, tên đường")
+
+
 class FingerPrintResponse(BaseSchema):
     image_url: str = Field(..., description='Ảnh bàn tay')
     hand_side: DropdownResponse = Field(..., description='Loại bàn tay')
