cbcf5e7a83db6e1394d791ec9562d07b3ee6005a
cuonglv4 <cuonglv4@scb.com.vn>
2021-10-19 11:03:57 +0700

    [add](#39): add signature

--- app/api/v1/endpoints/cif/basic_information/identity/signature/view.py
+++ app/api/v1/endpoints/cif/basic_information/identity/signature/view.py
@@ -4,6 +4,12 @@ from starlette import status
 from app.api.base.schema import ResponseData
 from app.api.base.swagger import swagger_response
 from app.api.v1.dependencies.authenticate import get_current_user_from_header
+from app.api.v1.endpoints.cif.basic_information.identity.signature.controller import (
+    CtrSignature
+)
+from app.api.v1.endpoints.cif.basic_information.identity.signature.schema import (
+    SignaturesSuccessResponse
+)
 from app.api.v1.endpoints.user.schema import UserInfoResponse
 
 router = APIRouter()
@@ -26,9 +32,9 @@ async def view_create_identity_document(current_user=Depends(get_current_user_fr
 @router.get(
     path="/",
     name="1. GTĐD - D. Chữ ký",
-    description="Detail",
+    description="Lấy dữ liệu tab `CHỮ KÝ` của khách hàng",
     responses=swagger_response(
-        response_model=ResponseData[UserInfoResponse],
+        response_model=ResponseData[SignaturesSuccessResponse],
         success_status_code=status.HTTP_200_OK
     )
 )
@@ -36,5 +42,5 @@ async def view_retrieve_user(
         cif_id: str = Path(...),
         current_user=Depends(get_current_user_from_header())
 ):
-    data = {'cif_id': cif_id}
-    return ResponseData[UserInfoResponse](**data)
+    signature_data = await CtrSignature(current_user).ctr_get_signature(cif_id)
+    return ResponseData[SignaturesSuccessResponse](**signature_data)
