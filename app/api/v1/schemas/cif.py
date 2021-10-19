f85dbfc7a62070cf090cf49a504a13e3e314514a
aitcq <aitcq@scb.com.vn>
2021-10-19 14:21:06 +0700

    rechange to dev

--- app/api/v1/schemas/cif.py
+++ app/api/v1/schemas/cif.py
@@ -1,10 +1,9 @@
 from pydantic import Field
 
 from app.api.base.schema import BaseSchema
-from app.api.v1.schemas.utils import DropdownResponse, DropdownRequest
+from app.api.v1.schemas.utils import DropdownRequest, DropdownResponse
 
 
-# III. Phân tích OCR -> 3. Thông tin địa chỉ -> Nơi thường trú/ Địa chỉ liên hệ (CMND)
 class AddressResponse(BaseSchema):
     province: DropdownResponse = Field(..., description="Tỉnh/Thành phố")
     district: DropdownResponse = Field(..., description="Quận/Huyện")
@@ -12,11 +11,11 @@ class AddressResponse(BaseSchema):
     number_and_street: str = Field(..., description="Số nhà, tên đường")
 
 
-# III. Phân tích OCR -> 2. Thông tin địa chỉ -> Chi tiết từng địa chỉ
-class AddressRequest(AddressResponse):
-    province: DropdownRequest = Field(..., description="Tỉnh/Thành phố")
-    district: DropdownRequest = Field(..., description="Quận/Huyện")
-    ward: DropdownRequest = Field(..., description="Phường/Xã")
+class AddressRequest(BaseSchema):
+    province: DropdownResponse = Field(..., description="Tỉnh/Thành phố")
+    district: DropdownResponse = Field(..., description="Quận/Huyện")
+    ward: DropdownResponse = Field(..., description="Phường/Xã")
+    number_and_street: str = Field(..., description="Số nhà, tên đường")
 
 
 class FingerPrintResponse(BaseSchema):
