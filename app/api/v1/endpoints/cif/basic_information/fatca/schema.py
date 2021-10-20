f84e923087fe07d184c874cf1b480313290b6b44
cuonglv4 <cuonglv4@scb.com.vn>
2021-10-20 15:44:49 +0700

    [add](#49): add post fatca data

--- app/api/v1/endpoints/cif/basic_information/fatca/schema.py
+++ app/api/v1/endpoints/cif/basic_information/fatca/schema.py
@@ -4,8 +4,41 @@ from typing import List
 from pydantic import Field
 
 from app.api.base.schema import BaseSchema
-from app.api.v1.schemas.utils import DropdownResponse
+from app.api.v1.schemas.utils import DropdownRequest, DropdownResponse
 
+########################################################################################################################
+# Request
+########################################################################################################################
+
+
+class CategoryDropdownRequest(DropdownRequest):
+    active_flag: bool = Field(False, description='`False`: Có. `True`: Không')
+
+
+class DocumentsRequest(BaseSchema):
+    id: str = Field(..., description='Mã biểu mẫu')
+    name: str = Field(..., description='Tên biểu mẫu')
+    url: str = Field(..., description='Đường dẫn biểu mẫu')
+    version: str = Field(..., description='Phiên bản biểu mẫu')
+    content_type: str = Field(..., description='Loại biểu mẫu')
+    size: str = Field(..., description='Kích thước biểu mẫu')
+    folder: str = Field(..., description='Thư mục biểu mẫu')
+    note: str = Field(..., description='Mô tả biểu mẫu')
+
+
+class DocumentsListRequest(BaseSchema):
+    language_type: DropdownRequest = Field(..., description='Ngôn ngữ biểu mẫu')
+    documents: List[DocumentsRequest] = Field(..., description='Thông tin biểu mẫu `FATCA`')
+
+
+class FatcaRequest(BaseSchema):
+    fatca_information: List[CategoryDropdownRequest] = Field(..., description='Danh mục `FATCA`')
+    documents_list: List[DocumentsListRequest] = Field(..., description='Danh sách biểu mẫu `FATCA`')
+
+
+########################################################################################################################
+# Response
+########################################################################################################################
 
 class CategoryDropdownResponse(DropdownResponse):
     active_flag: bool = Field(False, description='`False`: Có. `True`: Không')
@@ -27,11 +60,11 @@ class DocumentsResponse(BaseSchema):
     note: str = Field(..., description='Mô tả biểu mẫu')
 
 
-class DocumentListResponse(BaseSchema):
+class DocumentsListResponse(BaseSchema):
     language_type: DropdownResponse = Field(..., description='Ngôn ngữ biểu mẫu')
     documents: List[DocumentsResponse] = Field(..., description='Thông tin biểu mẫu `FATCA`')
 
 
 class FatcaResponse(BaseSchema):
     fatca_information: List[CategoryDropdownResponse] = Field(..., description='Danh mục `FATCA`')
-    documents_list: List[DocumentListResponse] = Field(..., description='Danh sách biểu mẫu `FATCA`')
+    documents_list: List[DocumentsListResponse] = Field(..., description='Danh sách biểu mẫu `FATCA`')
