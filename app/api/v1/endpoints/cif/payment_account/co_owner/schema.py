29edf8fcb2107f66e1a53759bdaeffeeb2ea7dac
cuonglv4 <cuonglv4@scb.com.vn>
2021-10-22 14:22:32 +0700

    [add](#66): add save co_owner

--- app/api/v1/endpoints/cif/payment_account/co_owner/schema.py
+++ app/api/v1/endpoints/cif/payment_account/co_owner/schema.py
@@ -4,7 +4,7 @@ from typing import List
 from pydantic import Field
 
 from app.api.base.schema import BaseSchema
-from app.api.v1.schemas.utils import DropdownResponse
+from app.api.v1.schemas.utils import DropdownRequest, DropdownResponse
 
 ############################################################
 # Response
@@ -66,3 +66,29 @@ class AccountHolderSuccessResponse(BaseSchema):
     number_of_joint_account_holder: int = Field(..., description='Số lượng đồng sở hữu')
     joint_account_holders: List[AccountHolderResponse] = Field(..., description='Thông tin cá nhân')
     agreement_authorization: List[AgreementAuthorResponse] = Field(..., description='Danh mục thỏa thuận và uỷ quyền')
+
+
+############################################################
+# Request
+############################################################
+
+class AccountRequest(BaseSchema):
+    cif_number: str = Field(..., description='Số CIF của đồng sở hữu')
+
+
+class SignatureAgreementAuthorRequest(BaseSchema):
+    id: str = Field(..., description='Mã định danh của đồng sở hữu')
+    full_name_vn: str = Field(..., description='Tên tiếng việt của đồng sở hữu')
+
+
+class AgreementAuthorRequest(BaseSchema):
+    id: str = Field(..., description='Mã danh mục thỏa thuận và uỷ quyền')
+    agreement_flag: bool = Field(..., description='Thỏa thuận chữ ký các hồ sơ chứng từ.`True`: Có , `False`: Không')
+    method_sign: DropdownRequest = Field(..., description='Phương thức ký')
+    signature_list: List[SignatureAgreementAuthorRequest] = Field(..., description='Chữ ký của đồng sở hữu')
+
+
+class AccountHolderRequest(BaseSchema):
+    joint_account_holder_flag: bool = Field(..., description='Có đồng chủ sở hữu. `True`: Có , `False`: Không')
+    joint_account_holders: List[AccountRequest] = Field(..., description='Danh sách các đồng sở hữu')
+    agreement_authorization: List[AgreementAuthorRequest] = Field(..., description='Danh mục thỏa thuận và ủy quyền')
