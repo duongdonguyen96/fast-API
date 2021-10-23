f26d9807852391fc4c171dea2f10a4c1ee9b614a
aitcq <aitcq@scb.com.vn>
2021-10-23 10:33:05 +0700

    [add]: common relationship

--- app/api/v1/schemas/cif.py
+++ app/api/v1/schemas/cif.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from pydantic import Field
 
 from app.api.base.schema import BaseSchema
@@ -28,3 +30,42 @@ class FingerPrintRequest(BaseSchema):
     image_url: str = Field(..., description="URL hình ảnh vân tay")
     hand_side: DropdownRequest = Field(..., description="Bàn tay")
     finger_type: DropdownRequest = Field(..., description="Ngón tay")
+
+
+########################################################################################################################
+# Phần này đùng chung cho Người giám hộ, Mối quan hệ với khách hàng
+########################################################################################################################
+# I. Thông tin cơ bản
+class BasicInformationResponse(BaseSchema):
+    cif_number: str = Field(..., description="Số CIF")
+    customer_relationship: DropdownResponse = Field(..., description="Mối quan hệ với khách hàng")
+    full_name_vn: str = Field(..., description="Họ và tên")
+    date_of_birth: date = Field(..., description="Ngày sinh")
+    gender: DropdownResponse = Field(..., description="Giới tính")
+    nationality: DropdownResponse = Field(..., description="Quốc tịch")
+    telephone_number: str = Field(..., description="Số ĐT bàn")
+    mobile_number: str = Field(..., description="Số ĐTDĐ")
+    email: str = Field(..., description="Email")
+
+
+# II. Giấy tờ định danh
+class IdentityDocumentResponse(BaseSchema):
+    identity_number: str = Field(..., description="Số CMND/CCCD/Hộ chiếu")
+    issued_date: date = Field(..., description="Ngày cấp")
+    expired_date: date = Field(..., description="Ngày hết hạn")
+    place_of_issue: DropdownResponse = Field(..., description="Nơi cấp")
+
+
+# III. Thông tin địa chỉ
+class AddressInformationResponse(BaseSchema):
+    resident_address: AddressResponse = Field(..., description="Địa chỉ thường trú")
+    contact_address: AddressResponse = Field(..., description="Địa chỉ liên hệ")
+
+
+# Thông tin dùng chung cho Mối quan hệ khách hàng, Người giám hộ
+class RelationshipResponse(BaseSchema):
+    id: str = Field(..., description="ID")
+    avatar_url: str = Field(..., description="URL avatar")
+    basic_information: BasicInformationResponse = Field(..., description="I. Thông tin cơ bản")
+    identity_document: IdentityDocumentResponse = Field(..., description="II. Giấy tờ định danh")
+    address_information: AddressInformationResponse = Field(..., description="III. Thông tin địa chỉ")
