262875f6cdf74ab7492c38e8fcf013abcf3a1130
cuonglv4 <cuonglv4@scb.com.vn>
2021-10-26 11:29:51 +0700

    [fix](#69): fix conflict develop

--- app/api/v1/endpoints/cif/e_banking/controller.py
+++ app/api/v1/endpoints/cif/e_banking/controller.py
@@ -1,11 +1,23 @@
 from app.api.base.controller import BaseController
 from app.api.v1.endpoints.cif.e_banking.repository import (
     repos_get_detail_reset_password, repos_get_e_banking_data,
-    repos_get_list_balance_payment_account
+    repos_get_list_balance_payment_account, repos_save_e_banking_data
 )
+from app.api.v1.endpoints.cif.e_banking.schema import EBankingRequest
 
 
 class CtrEBanking(BaseController):
+    async def ctr_save_e_banking(self, cif_id: str, e_banking: EBankingRequest):
+        e_banking_data = self.call_repos(
+            await repos_save_e_banking_data(
+                cif_id,
+                e_banking,
+                created_by=self.current_user.full_name_vn
+            )
+        )
+
+        return self.response(data=e_banking_data)
+
     async def ctr_e_banking(self, cif_id: str):
         e_banking_data = self.call_repos(await repos_get_e_banking_data(cif_id))
 
