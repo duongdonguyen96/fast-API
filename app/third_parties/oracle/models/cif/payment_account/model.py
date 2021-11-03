f18a6004ec8b786a333cd50b85f94706eff9f2ea
Võ Văn Dương <duongvv@minerva.vn>
2021-11-03 17:18:23 +0700

    [fix](#82): spelling mistake

--- app/third_parties/oracle/models/cif/payment_account/model.py
+++ app/third_parties/oracle/models/cif/payment_account/model.py
@@ -17,8 +17,9 @@ class CasaAccount(Base):
     acc_type_id = Column(ForeignKey('crm_acc_type.acc_type_id'), nullable=False,
                          comment='Mã loại nhóm sản phẩm (gói) tài khoản')
     acc_class_id = Column(ForeignKey('crm_acc_class.acc_class_id'), nullable=False, comment='Mã loại hình tài khoản')
-    acc_structrure_type_id = Column(ForeignKey('crm_acc_structure_type.acc_structure_type_id'), nullable=False,
-                                    comment='Mã loại kết cấu tài khoản')
+    acc_structure_type_id = Column('acc_structrure_type_id',
+                                   ForeignKey('crm_acc_structure_type.acc_structure_type_id'), nullable=False,
+                                   comment='Mã loại kết cấu tài khoản')
     staff_type_id = Column(VARCHAR(36), nullable=False, comment='Mã Danh mục Loại nhân viên giới thiệu')
     acc_salary_org_name = Column(VARCHAR(100), comment='Tên tài khoản chi lương nguồn')
     acc_salary_org_acc = Column(VARCHAR(100), comment='Số tài khoản chi lương nguồn')
