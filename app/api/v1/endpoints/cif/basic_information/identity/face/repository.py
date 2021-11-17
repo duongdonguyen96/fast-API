26dbd672b0b3462f46d57b23dc7d5d5c46393a3d
cuonglv4 <cuonglv4@scb.com.vn>
2021-11-17 14:03:09 +0700

    [fix](#36): fix query and refactor code

--- app/api/v1/endpoints/cif/basic_information/identity/face/repository.py
+++ app/api/v1/endpoints/cif/basic_information/identity/face/repository.py
@@ -1,4 +1,5 @@
-from sqlalchemy import desc, select
+from sqlalchemy import and_, desc, select
+from sqlalchemy.orm import Session
 
 from app.api.base.repository import ReposReturn
 from app.third_parties.oracle.models.cif.basic_information.identity.model import (
@@ -11,7 +12,7 @@ from app.utils.error_messages import ERROR_CIF_ID_NOT_EXIST
 from app.utils.functions import datetime_to_date
 
 
-async def repos_get_list_face(cif_id: str, session) -> ReposReturn:
+async def repos_get_list_face(cif_id: str, session: Session) -> ReposReturn:
     query_data = session.execute(
         select(
             Customer,
@@ -24,36 +25,43 @@ async def repos_get_list_face(cif_id: str, session) -> ReposReturn:
             CustomerIdentityImage, CustomerIdentity.id == CustomerIdentityImage.identity_id
         ).join(
             CustomerCompareImage, CustomerIdentityImage.id == CustomerCompareImage.identity_image_id
-        ).filter(Customer.id == cif_id).order_by(desc(CustomerIdentityImage.maker_at))
+        ).filter(
+            and_(
+                Customer.id == cif_id,
+                CustomerIdentityImage.finger_type_id.is_(None),
+                CustomerIdentityImage.hand_side_id.is_(None)
+            )
+        ).order_by(desc(CustomerIdentityImage.maker_at))
     ).all()
-
+    print('query', query_data, len(query_data))
     if not query_data:
         return ReposReturn(is_error=True, msg=ERROR_CIF_ID_NOT_EXIST, loc="cif_id")
 
     data_response = []
     data = {}
-    for customer, customer_identity, customer_identity_image, customer_compare_image in query_data:
+    for _, _, customer_identity_image, customer_compare_image in query_data:
 
-        if data.get('created_date') == datetime_to_date(customer_identity_image.maker_at):
-            data['faces'].append({
+        if data.get(datetime_to_date(customer_identity_image.maker_at)):
+            data[datetime_to_date(customer_identity_image.maker_at)].append(
+                {
+                    "identity_image_id": customer_identity_image.id,
+                    "image_url": customer_identity_image.image_url,
+                    "created_at": customer_identity_image.maker_at,
+                    "similar_percent": customer_compare_image.similar_percent
+                }
+            )
+        else:
+            data[datetime_to_date(customer_identity_image.maker_at)] = [{
                 "identity_image_id": customer_identity_image.id,
                 "image_url": customer_identity_image.image_url,
                 "created_at": customer_identity_image.maker_at,
                 "similar_percent": customer_compare_image.similar_percent
-            })
-        else:
-            data = {
-                "created_date": datetime_to_date(customer_identity_image.maker_at),
-                "faces": [
-                    {
-                        "identity_image_id": customer_identity_image.id,
-                        "image_url": customer_identity_image.image_url,
-                        "created_at": customer_identity_image.maker_at,
-                        "similar_percent": customer_compare_image.similar_percent
-                    }
-                ]
-            }
-        if data not in data_response:
-            data_response.append(data)
+            }]
+
+    for data_key, data_value in data.items():
+        data_response.append({
+            'created_date': data_key,
+            'faces': data_value
+        })
 
     return ReposReturn(data=data_response)
