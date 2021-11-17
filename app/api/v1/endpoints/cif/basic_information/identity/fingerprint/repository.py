e474578463109262e45d6898c5f0d0f79e1391bd
cuonglv4 <cuonglv4@scb.com.vn>
2021-11-17 09:31:36 +0700

    [add](#37): add fingerprint query database\

--- app/api/v1/endpoints/cif/basic_information/identity/fingerprint/repository.py
+++ app/api/v1/endpoints/cif/basic_information/identity/fingerprint/repository.py
@@ -1,6 +1,17 @@
+from sqlalchemy import select
+
 from app.api.base.repository import ReposReturn
 from app.api.v1.endpoints.cif.basic_information.identity.fingerprint.schema import (
     TwoFingerPrintRequest
+)
+from app.third_parties.oracle.models.cif.basic_information.identity.model import (
+    CustomerIdentity, CustomerIdentityImage
+)
+from app.third_parties.oracle.models.cif.basic_information.model import (
+    Customer
+)
+from app.third_parties.oracle.models.master_data.identity import (
+    FingerType, HandSide
 )
 from app.utils.constant.cif import CIF_ID_TEST
 from app.utils.error_messages import ERROR_CIF_ID_NOT_EXIST
@@ -17,91 +28,67 @@ async def repos_save_fingerprint(cif_id: str, finger_request: TwoFingerPrintRequ
     })
 
 
-async def repos_get_data_finger(cif_id: str) -> ReposReturn:
-    if cif_id != CIF_ID_TEST:
-        return ReposReturn(is_error=True, msg=ERROR_CIF_ID_NOT_EXIST, loc='cif_id')
+async def repos_get_data_finger(cif_id: str, session) -> ReposReturn:
 
-    return ReposReturn(data={
-        "fingerprint_1": [
-            {
-                "image_url": "https://example.com/abc.png",
-                "hand_side": {
-                    "id": "1",
-                    "code": "taytrai",
-                    "name": "bàn tay trái"
-                },
-                "finger_type": {
-                    "id": "1",
-                    "code": "ngontro",
-                    "name": "ngón trỏ"
+    query_data = session.execute(
+        select(
+            Customer,
+            CustomerIdentity,
+            CustomerIdentityImage,
+            HandSide,
+            FingerType
+        ).join(
+            CustomerIdentity, Customer.id == CustomerIdentity.customer_id
+        ).join(
+            CustomerIdentityImage, CustomerIdentity.id == CustomerIdentityImage.identity_id
+        ).join(
+            HandSide, CustomerIdentityImage.hand_side_id == HandSide.id
+        ).join(
+            FingerType, CustomerIdentityImage.finger_type_id == FingerType.id
+        ).filter(Customer.id == cif_id).order_by(CustomerIdentityImage.finger_type_id)
+    ).all()
+    if not query_data:
+        return ReposReturn(is_error=True, msg=ERROR_CIF_ID_NOT_EXIST, loc="cif_id")
+
+    fingerprint_1 = []
+    fingerprint_2 = []
+    data_response = {
+        'fingerprint_1': fingerprint_1,
+        'fingerprint_2': fingerprint_2,
+    }
+
+    for customer, customer_identity, customer_identity_image, hand_side, finger_print in query_data:
+        if hand_side.id == "1":
+            fingerprint_1.append(
+                {
+                    'image_url': customer_identity_image.image_url,
+                    'hand_side': {
+                        'id': hand_side.id,
+                        'code': hand_side.code,
+                        'name': hand_side.name
+                    },
+                    'finger_type': {
+                        'id': finger_print.id,
+                        'code': finger_print.code,
+                        'name': finger_print.name
+                    }
                 }
-            },
-            {
-                "image_url": "https://example.com/abc.png",
-                "hand_side": {
-                    "id": "1",
-                    "code": "taytrai",
-                    "name": "bàn tay trái"
-                },
-                "finger_type": {
-                    "id": "2",
-                    "code": "ngongiua",
-                    "name": "ngón giữa"
+            )
+        else:
+            fingerprint_2.append(
+                {
+                    'image_url': customer_identity_image.image_url,
+                    'hand_side': {
+                        'id': hand_side.id,
+                        'code': hand_side.code,
+                        'name': hand_side.name
+                    },
+                    'finger_type': {
+                        'id': finger_print.id,
+                        'code': finger_print.code,
+                        'name': finger_print.name
+                    }
                 }
-            },
-            {
-                "image_url": "https://example.com/abc.png",
-                "hand_side": {
-                    "id": "1",
-                    "code": "taytrai",
-                    "name": "bàn tay trái"
-                },
-                "finger_type": {
-                    "id": "3",
-                    "code": "ngoncai",
-                    "name": "ngón cái"
-                }
-            }
-        ],
-        "fingerprint_2": [
-            {
-                "image_url": "https://example.com/abc.png",
-                "hand_side": {
-                    "id": "2",
-                    "code": "tayphai",
-                    "name": "bàn tay phải"
-                },
-                "finger_type": {
-                    "id": "1",
-                    "code": "ngontro",
-                    "name": "ngón trỏ"
-                }
-            },
-            {
-                "image_url": "https://example.com/abc.png",
-                "hand_side": {
-                    "id": "2",
-                    "code": "tayphai",
-                    "name": "bàn tay phải"
-                },
-                "finger_type": {
-                    "id": "2",
-                    "code": "ngongiua",
-                    "name": "ngón giữa"
-                }
-            },
-            {
-                "image_url": "https://example.com/abc.png",
-                "hand_side": {
-                    "id": "2",
-                    "code": "tayphai",
-                    "name": "bàn tay phẩi"
-                },
-                "finger_type": {
-                    "id": "3",
-                    "code": "ngoncai",
-                    "name": "ngón cái"
-                }
-            }
-        ]
-    })
+            )
+
+    return ReposReturn(data=data_response)
