bc40a8e730bd5994ba5b3173ed57c911a6b8bb73
thinhnx <thinhnx@Minerva.vn>
2021-11-18 14:20:27 +0700

    [refactor](#56): refactor code

--- app/api/v1/endpoints/cif/other_information/repository.py
+++ app/api/v1/endpoints/cif/other_information/repository.py
@@ -14,7 +14,7 @@ from app.third_parties.oracle.models.cif.other_information.model import (
 from app.third_parties.oracle.models.master_data.others import (
     HrmEmployee, StaffType
 )
-from app.utils.constant.cif import CIF_ID_TEST, STAFF_TYPE_CODE
+from app.utils.constant.cif import BUSINESS_STAFF_TYPE_CODE, CIF_ID_TEST
 from app.utils.error_messages import ERROR_CIF_ID_NOT_EXIST
 from app.utils.functions import now
 
@@ -24,11 +24,11 @@ async def repos_other_info(cif_id: str, session: Session) -> ReposReturn:
         select(
             Customer, StaffType, HrmEmployee
         ).join(
-            CustomerEmployee, CustomerEmployee.customer_id == Customer.id
+            CustomerEmployee, Customer.id == CustomerEmployee.customer_id
         ).join(
-            StaffType, StaffType.id == CustomerEmployee.staff_type_id, isouter=True
+            StaffType, CustomerEmployee.staff_type_id == StaffType.id
         ).join(
-            HrmEmployee, CustomerEmployee.employee_id == HrmEmployee.id, isouter=True
+            HrmEmployee, CustomerEmployee.employee_id == HrmEmployee.id
         ).filter(Customer.id == cif_id)
     )
     customer_employee = customer_employee_engine.all()
@@ -37,24 +37,22 @@ async def repos_other_info(cif_id: str, session: Session) -> ReposReturn:
         return ReposReturn(is_error=True, msg=ERROR_CIF_ID_NOT_EXIST, loc="cif_id")
 
     sale_staff = None
-    legal_agreement_flag = ""
-    advertising_marketing_flag = ""
     indirect_sale_staff = None
 
-    for customer, staff_type, employee in customer_employee:
-        legal_agreement_flag = customer.legal_agreement_flag
-        advertising_marketing_flag = customer.advertising_marketing_flag
-        if staff_type.code == STAFF_TYPE_CODE:
+    # vị trí của customer trong query ở vị trí 0
+    legal_agreement_flag = customer_employee[0][0].legal_agreement_flag
+    advertising_marketing_flag = customer_employee[0][0].advertising_marketing_flag
+
+    for _, staff_type, employee in customer_employee:
+        if staff_type.code == BUSINESS_STAFF_TYPE_CODE:
             sale_staff = {
-                "id": staff_type.id,
-                "code": staff_type.code,
-                "name": employee.fullname_vn
+                "id": employee.id,
+                "fullname_vn": employee.fullname_vn
             }
         else:
             indirect_sale_staff = {
-                "id": staff_type.id,
-                "code": staff_type.code,
-                "name": employee.fullname_vn
+                "id": employee.id,
+                "fullname_vn": employee.fullname_vn
             }
 
     return ReposReturn(data={
