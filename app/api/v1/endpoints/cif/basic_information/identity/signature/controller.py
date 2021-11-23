e7b36f49ba97404171c72fe0f21df58217f2a822
cuonglv4 <cuonglv4@scb.com.vn>
2021-11-23 13:31:18 +0700

    [refactor](#41): refactor code signature

--- app/api/v1/endpoints/cif/basic_information/identity/signature/controller.py
+++ app/api/v1/endpoints/cif/basic_information/identity/signature/controller.py
@@ -1,23 +1,35 @@
 from app.api.base.controller import BaseController
 from app.api.v1.endpoints.cif.basic_information.identity.signature.repository import (
-    repos_get_identity_id, repos_get_signature_data, repos_get_type_id,
-    repos_save_signature
+    repos_get_signature_data, repos_save_signature
 )
 from app.api.v1.endpoints.cif.basic_information.identity.signature.schema import (
     SignaturesRequest
 )
+from app.api.v1.endpoints.cif.repository import (
+    repos_get_image_type, repos_get_initializing_customer,
+    repos_get_last_identity
+)
+from app.utils.constant.cif import IMAGE_TYPE_SIGNATURE
 from app.utils.functions import now
 
 
 class CtrSignature(BaseController):
     async def ctr_save_signature(self, cif_id: str, signatures: SignaturesRequest):
+        # check cif đang tạo
+        self.call_repos(await repos_get_initializing_customer(cif_id=cif_id, session=self.oracle_session))
 
-        type_id = self.call_repos(await repos_get_type_id(self.oracle_session))
-        identity = self.call_repos(await repos_get_identity_id(cif_id, self.oracle_session))
+        image_type = self.call_repos(
+            await repos_get_image_type(
+                image_type=IMAGE_TYPE_SIGNATURE,
+                session=self.oracle_session
+            )
+        )
+
+        identity = self.call_repos(await repos_get_last_identity(cif_id=cif_id, session=self.oracle_session))
 
         list_data_insert = [{
             'identity_id': identity.id,
-            'image_type_id': type_id.code,
+            'image_type_id': image_type.code,
             'image_url': signature.image_url,
             'hand_side_id': None,
             'finger_type_id': None,
@@ -30,9 +42,9 @@ class CtrSignature(BaseController):
 
         data = self.call_repos(
             await repos_save_signature(
-                cif_id,
-                list_data_insert,
-                self.oracle_session,
+                cif_id=cif_id,
+                list_data_insert=list_data_insert,
+                session=self.oracle_session,
                 created_by=self.current_user.full_name_vn
             )
         )
