1c666796a45cd75783a1cd17831505284bea2a7a
cuonglv4 <cuonglv4@scb.com.vn>
2021-11-23 15:35:46 +0700

    [refactor](#41): controller signature and fingerprint

--- app/api/v1/endpoints/cif/basic_information/identity/signature/controller.py
+++ app/api/v1/endpoints/cif/basic_information/identity/signature/controller.py
@@ -6,35 +6,35 @@ from app.api.v1.endpoints.cif.basic_information.identity.signature.schema import
     SignaturesRequest
 )
 from app.api.v1.endpoints.cif.repository import (
-    repos_get_image_type, repos_get_initializing_customer,
-    repos_get_last_identity
+    repos_get_initializing_customer, repos_get_last_identity
 )
-from app.utils.constant.cif import IMAGE_TYPE_SIGNATURE
+from app.utils.constant.cif import (
+    ACTIVE_FLAG_CREATE_SIGNATURE, IMAGE_TYPE_SIGNATURE
+)
 from app.utils.functions import now
 
 
 class CtrSignature(BaseController):
     async def ctr_save_signature(self, cif_id: str, signatures: SignaturesRequest):
+        # check len signature request
+        if len(signatures.signatures) != 2:
+            return self.response_exception(
+                msg='signature must be equal 2',
+                loc='ERROR_SAVE_SIGNATURE',
+                detail='Can not save signature'
+            )
         # check cif đang tạo
         self.call_repos(await repos_get_initializing_customer(cif_id=cif_id, session=self.oracle_session))
-
-        image_type = self.call_repos(
-            await repos_get_image_type(
-                image_type=IMAGE_TYPE_SIGNATURE,
-                session=self.oracle_session
-            )
-        )
-
         identity = self.call_repos(await repos_get_last_identity(cif_id=cif_id, session=self.oracle_session))
 
         list_data_insert = [{
             'identity_id': identity.id,
-            'image_type_id': image_type.code,
+            'image_type_id': IMAGE_TYPE_SIGNATURE,
             'image_url': signature.image_url,
             'hand_side_id': None,
             'finger_type_id': None,
             'vector_data': None,
-            'active_flag': 1,
+            'active_flag': ACTIVE_FLAG_CREATE_SIGNATURE,
             'maker_id': self.current_user.user_id,
             'maker_at': now(),
             'identity_image_front_flag': None
