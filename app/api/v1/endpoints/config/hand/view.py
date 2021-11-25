3904904caf91d6596e22eb2c0633f1d9f4f2c30d
thinhnx <thinhnx@Minerva.vn>
2021-11-25 10:50:59 +0700

    [add](#55):get api config ( not finish)

---
+++ app/api/v1/endpoints/config/hand/view.py
@@ -0,0 +1,44 @@
+from typing import List
+
+from fastapi import APIRouter, Depends
+from starlette import status
+
+from app.api.base.schema import ResponseData
+from app.api.base.swagger import swagger_response
+from app.api.v1.dependencies.authenticate import get_current_user_from_header
+from app.api.v1.endpoints.config.hand.controller import CtrConfigHand
+from app.api.v1.schemas.utils import DropdownResponse
+
+router = APIRouter()
+
+
+@router.get(
+    path="/hand-side/",
+    name="Hand Side",
+    description="Lấy dữ liệu config bàn tay",
+    responses=swagger_response(
+        response_model=ResponseData[List[DropdownResponse]],
+        success_status_code=status.HTTP_200_OK
+    )
+)
+async def view_hand_side_info(
+        current_user=Depends(get_current_user_from_header())
+):
+    hand_side_info = await CtrConfigHand(current_user).ctr_hand_side_info()
+    return ResponseData[List[DropdownResponse]](**hand_side_info)
+
+
+@router.get(
+    path="/finger-printer/",
+    name="Finger Printer",
+    description="Lấy dữ liệu config các loại vân tay",
+    responses=swagger_response(
+        response_model=ResponseData[List[DropdownResponse]],
+        success_status_code=status.HTTP_200_OK
+    )
+)
+async def view_finger_printer_info(
+        current_user=Depends(get_current_user_from_header())
+):
+    finger_printer_info = await CtrConfigHand(current_user).ctrl_finger_printer()
+    return ResponseData[List[DropdownResponse]](**finger_printer_info)
--- app/api/v1/endpoints/config/router.py
+++ app/api/v1/endpoints/config/router.py
@@ -1,3 +1,10 @@
 from fastapi import APIRouter
 
+from app.api.v1.endpoints.config.cif_information import view as views_cif_info
+from app.api.v1.endpoints.config.hand import view as views_hand_side_info
+
 router = APIRouter()
+
+router.include_router(router=views_hand_side_info.router)
+
+router.include_router(router=views_cif_info.router)
