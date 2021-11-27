8357128337639321803cb825a7a1f662b8714fc1
Võ Văn Dương <duongvv@minerva.vn>
2021-11-27 15:13:34 +0700

    [add]: Vietnamese converter

--- app/utils/vietnamese_converter.py
+++ app/utils/vietnamese_converter.py
@@ -1,5 +1,5 @@
 import re
-from typing import Union
+from typing import Optional, Tuple, Union
 
 patterns = {
     '[àáảãạăắằẵặẳâầấậẫẩ]': 'a',
@@ -12,7 +12,7 @@ patterns = {
 }
 
 
-def vietnamese_converted(text: str) -> str:
+def convert_to_unsigned_vietnamese(text: str) -> str:
     """
     Convert from 'Tiếng Việt' into 'Tieng Viet'
     """
@@ -24,7 +24,8 @@ def vietnamese_converted(text: str) -> str:
     return output
 
 
-def split_name(full_name: str) -> Union[tuple[str, str, str], tuple[str, None, str], None]:
+# TODO: sửa lại hàm này first name là tên
+def split_name(full_name: str) -> Union[Tuple[str, str, str], Tuple[str, None, str], Tuple[None, None, None]]:
     """
     Split full_name into first_name, middle_name, last_name
     """
@@ -34,16 +35,15 @@ def split_name(full_name: str) -> Union[tuple[str, str, str], tuple[str, None, s
     elif len(data) == 2:
         return data[0], None, data[-1]
     else:
-        return None
+        return None, None, None
 
 
-def make_short_name(full_name: str):
+# TODO: sửa lại hàm này, middle name có thể rỗng
+def make_short_name(first_name: str, middle_name: Optional[str], last_name: str):
     """
     Make full name to short name
     Example: Lê Phương Thảo => thaolp
     """
-    first_name, middle_name, last_name = split_name(full_name)
-
     if len(middle_name.split(" ")) > 1:
         middle_short_name = [middle_name[0] for middle_name in middle_name.split(" ")]
         middle_name = "".join(middle_short_name)
