df8d833d5e21dff0114daa403fa9f2b03b19d728
Võ Văn Dương <duongvv@minerva.vn>
2021-12-01 11:39:01 +0700

    [fix](#82): change scale

--- app/third_parties/oracle/models/cif/basic_information/guardian_and_relationship/model.py
+++ app/third_parties/oracle/models/cif/basic_information/guardian_and_relationship/model.py
@@ -20,7 +20,7 @@ class CustomerPersonalRelationship(Base):
     customer_id = Column(ForeignKey('crm_customer.customer_id'), nullable=False, comment='Mã khách hàng')
     customer_relationship_type_id = Column(ForeignKey('crm_cust_relationship_type.cust_relationship_type_id'),
                                            nullable=False, comment='Mã loại mối quan hệ khách hàng')
-    type = Column('cust_personal_relationship_type', NUMBER(1, 2, True), nullable=False,
+    type = Column('cust_personal_relationship_type', NUMBER(1, 0, True), nullable=False,
                   comment='Loại quan hệ khách hàng cá nhân (0: Người giám hộ, 1: Mối quan hệ khách hàng)')
     customer_personal_relationship_cif_number = Column('cust_personal_relationship_cif_num', VARCHAR(9),
                                                        comment='Số cif của Người giám hộ hoặc Mối quan hệ khách hàng')
