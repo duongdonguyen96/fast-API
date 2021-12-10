7c525111c3f4bc38a3f47029c37494d286fc48ba
Phạm Thành Đạt <datpt@minerva.vn>
2021-12-10 04:40:31 +0000

    [refactor] -  reset app/third_parties/oracle/base.py

--- app/third_parties/oracle/base.py
+++ app/third_parties/oracle/base.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
 from app.settings.database import ORACLE_CONFIG
 
 DATABASE_URL = "oracle+cx_oracle://{username}:{password}@{host}:{port}/?service_name={service_name}".format_map({
