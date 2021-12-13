7858fe71eff50f358cbbf4c9bdf47ee54d83966e
cuonglv4 <cuonglv4@scb.com.vn>
2021-12-13 09:22:13 +0700

    [refactor](#48): refactor code

--- app/api/v1/endpoints/cif/basic_information/fatca/controller.py
+++ app/api/v1/endpoints/cif/basic_information/fatca/controller.py
@@ -7,6 +7,9 @@ from app.api.v1.endpoints.cif.basic_information.fatca.schema import (
 )
 from app.api.v1.endpoints.cif.repository import repos_get_initializing_customer
 from app.third_parties.oracle.models.master_data.others import FatcaCategory
+from app.utils.constant.cif import (
+    LANGUAGE_ID_EN, LANGUAGE_ID_VN, LANGUAGE_TYPE_EN, LANGUAGE_TYPE_VN
+)
 from app.utils.functions import generate_uuid, now
 
 
@@ -81,4 +84,84 @@ class CtrFatca(BaseController):
     async def ctr_get_fatca(self, cif_id: str):
         fatca_data = self.call_repos(await repos_get_fatca_data(cif_id=cif_id, session=self.oracle_session))
 
-        return self.response(data=fatca_data)
+        fatca_information = {}
+
+        for customer_fatca, fatca_category, customer_fatca_document in fatca_data:
+            if fatca_category.id not in fatca_information:
+                fatca_information[fatca_category.id] = {
+                    "id": fatca_category.id,
+                    "code": fatca_category.code,
+                    "name": fatca_category.name,
+                    "select_flag": customer_fatca.value,
+                    "document_depend_language": {}
+                }
+            # check customer_fatca_document
+            if customer_fatca_document is not None:
+                document = {
+                    "id": customer_fatca_document.id,
+                    "name": customer_fatca_document.document_name,
+                    "url": customer_fatca_document.document_url,
+                    "active_flag": customer_fatca_document.active_flag,
+                    "document_language_type": customer_fatca_document.document_language_type,
+                    "version": customer_fatca_document.document_version,
+                    "content_type": "Word",  # TODO
+                    "size": "1MB",  # TODO
+                    "folder_name": "Khởi tạo CIF",  # TODO
+                    "created_by": "Nguyễn Phúc",  # TODO
+                    "created_at": customer_fatca_document.created_at,
+                    "updated_by": "Trần Bình Liên",  # TODO
+                    "updated_at": "2020-12-30 06:07:08",  # TODO
+                    "note": "Tài liệu quan trọng"  # TODO
+                }
+
+                if customer_fatca_document.document_language_type == LANGUAGE_ID_EN:
+                    fatca_information[fatca_category.id]["document_depend_language"][LANGUAGE_TYPE_EN] = document
+
+                if customer_fatca_document.document_language_type == LANGUAGE_ID_VN:
+                    fatca_information[fatca_category.id]["document_depend_language"][LANGUAGE_TYPE_VN] = document
+
+        # TODO : xét cứng dữ liệu language -> chưa thấy table lưu
+        en_documents = []
+        vi_documents = []
+        for fatca_category_id, fatca_category_data in fatca_information.items():
+            en_document = fatca_category_data['document_depend_language'].get(LANGUAGE_TYPE_EN)
+            vi_document = fatca_category_data['document_depend_language'].get(LANGUAGE_TYPE_VN)
+
+            en_documents.append(
+                {
+                    "id": fatca_category_data['id'],
+                    "code": fatca_category_data['code'],
+                    "name": fatca_category_data['name'],
+                    "document": en_document
+                }
+            )
+
+            vi_documents.append(
+                {
+                    "id": fatca_category_data['id'],
+                    "code": fatca_category_data['code'],
+                    "name": fatca_category_data['name'],
+                    "document": vi_document
+                }
+            )
+        return self.response(data={
+            "fatca_information": list(fatca_information.values()),
+            "document_information": [
+                {
+                    "language_type": {
+                        "id": LANGUAGE_ID_VN,
+                        "code": LANGUAGE_TYPE_VN,
+                        "name": "VN"
+                    },
+                    "documents": vi_documents
+                },
+                {
+                    "language_type": {
+                        "id": LANGUAGE_ID_EN,
+                        "code": LANGUAGE_TYPE_EN,
+                        "name": "EN"
+                    },
+                    "documents": en_documents
+                }
+            ]
+        })
--- app/api/v1/endpoints/cif/basic_information/fatca/repository.py
+++ app/api/v1/endpoints/cif/basic_information/fatca/repository.py
@@ -8,9 +8,6 @@ from app.third_parties.oracle.models.cif.basic_information.fatca.model import (
     CustomerFatca, CustomerFatcaDocument
 )
 from app.third_parties.oracle.models.master_data.others import FatcaCategory
-from app.utils.constant.cif import (
-    LANGUAGE_ID_EN, LANGUAGE_ID_VN, LANGUAGE_TYPE_EN, LANGUAGE_TYPE_VN
-)
 from app.utils.error_messages import ERROR_CIF_ID_NOT_EXIST
 
 
@@ -69,84 +66,4 @@ async def repos_get_fatca_data(cif_id: str, session: Session) -> ReposReturn:
     if not query_data_fatca:
         return ReposReturn(is_error=True, msg=ERROR_CIF_ID_NOT_EXIST, loc="cif_id")
 
-    fatca_information = {}
-
-    for customer_fatca, fatca_category, customer_fatca_document in query_data_fatca:
-        if fatca_category.id not in fatca_information:
-            fatca_information[fatca_category.id] = {
-                "id": fatca_category.id,
-                "code": fatca_category.code,
-                "name": fatca_category.name,
-                "select_flag": customer_fatca.value,
-                "document_depend_language": {}
-            }
-        # check customer_fatca_document
-        if customer_fatca_document is not None:
-            document = {
-                "id": customer_fatca_document.id,
-                "name": customer_fatca_document.document_name,
-                "url": customer_fatca_document.document_url,
-                "active_flag": customer_fatca_document.active_flag,
-                "version": customer_fatca_document.document_version,
-                "content_type": "Word",  # TODO
-                "size": "1MB",  # TODO
-                "folder_name": "Khởi tạo CIF",  # TODO
-                "created_by": "Nguyễn Phúc",  # TODO
-                "created_at": customer_fatca_document.created_at,
-                "updated_by": "Trần Bình Liên",  # TODO
-                "updated_at": "2020-12-30 06:07:08",  # TODO
-                "note": "Tài liệu quan trọng"  # TODO
-            }
-
-            if customer_fatca_document.document_language_type == LANGUAGE_TYPE_EN:
-                fatca_information[fatca_category.id]["document_depend_language"][LANGUAGE_TYPE_EN] = document
-
-            if customer_fatca_document.document_language_type == LANGUAGE_TYPE_VN:
-                fatca_information[fatca_category.id]["document_depend_language"][LANGUAGE_TYPE_VN] = document
-
-    # TODO : xét cứng dữ liệu language -> chưa thấy table lưu
-    en_documents = []
-    vi_documents = []
-    for fatca_category_id, fatca_category_data in fatca_information.items():
-        en_document = fatca_category_data['document_depend_language'].get(LANGUAGE_TYPE_EN)
-        vi_document = fatca_category_data['document_depend_language'].get(LANGUAGE_TYPE_VN)
-
-        en_documents.append(
-            {
-                "id": fatca_category_data['id'],
-                "code": fatca_category_data['code'],
-                "name": fatca_category_data['name'],
-                "document": en_document
-            }
-        )
-
-        vi_documents.append(
-            {
-                "id": fatca_category_data['id'],
-                "code": fatca_category_data['code'],
-                "name": fatca_category_data['name'],
-                "document": vi_document
-            }
-        )
-
-    return ReposReturn(data={
-        "fatca_information": list(fatca_information.values()),
-        "document_information": [
-            {
-                "language_type": {
-                    "id": LANGUAGE_ID_VN,
-                    "code": LANGUAGE_TYPE_VN,
-                    "name": "VN"
-                },
-                "documents": vi_documents
-            },
-            {
-                "language_type": {
-                    "id": LANGUAGE_ID_EN,
-                    "code": LANGUAGE_TYPE_EN,
-                    "name": "EN"
-                },
-                "documents": en_documents
-            }
-        ]
-    })
+    return ReposReturn(data=query_data_fatca)
