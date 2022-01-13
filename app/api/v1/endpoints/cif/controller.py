02d1d284d338f47f09f603b09e2ca9f335b48875
aitcq <aitcq@scb.com.vn>
2022-01-13 17:14:06 +0700

    [add](#64): Check exist CIF

--- app/api/v1/endpoints/cif/controller.py
+++ app/api/v1/endpoints/cif/controller.py
@@ -1,7 +1,11 @@
+import re
+
 from app.api.base.controller import BaseController
 from app.api.v1.endpoints.cif.repository import (
-    repos_customer_information, repos_get_cif_info, repos_profile_history
+    repos_check_exist_cif, repos_customer_information, repos_get_cif_info,
+    repos_profile_history
 )
+from app.utils.error_messages import ERROR_CIF_NUMBER_INVALID, MESSAGE_STATUS
 
 
 class CtrCustomer(BaseController):
@@ -21,3 +25,13 @@ class CtrCustomer(BaseController):
         customer_information = self.call_repos(await repos_customer_information(cif_id))
 
         return self.response(data=customer_information)
+
+    async def ctr_check_exist_cif(self, cif_number: str):
+        regex = re.search("[0-9]+", cif_number)
+        if not regex or len(regex.group()) != len(cif_number):
+            return self.response_exception(msg=ERROR_CIF_NUMBER_INVALID, detail=MESSAGE_STATUS[ERROR_CIF_NUMBER_INVALID])
+
+        check_exist_info = self.call_repos(
+            await repos_check_exist_cif(cif_number=cif_number)
+        )
+        return self.response(data=check_exist_info)
--- app/api/v1/endpoints/cif/view.py
+++ app/api/v1/endpoints/cif/view.py
@@ -8,6 +8,7 @@ from app.api.base.swagger import swagger_response
 from app.api.v1.dependencies.authenticate import get_current_user_from_header
 from app.api.v1.endpoints.cif.controller import CtrCustomer
 from app.api.v1.endpoints.cif.schema import (
+    CheckExistCIFRequest, CheckExistCIFSuccessResponse,
     CifCustomerInformationResponse, CifInformationResponse,
     CifProfileHistoryResponse
 )
@@ -64,3 +65,20 @@ async def view_customer(
 ):
     customer_information_data = await CtrCustomer(current_user).ctr_customer_information(cif_id)
     return ResponseData[CifCustomerInformationResponse](**customer_information_data)
+
+
+@router.post(
+    path="/check-exist/",
+    name="Kiểm tra tồn tại",
+    description="Kiểm tra CIF có tồn tại hay không",
+    responses=swagger_response(
+        response_model=ResponseData[CheckExistCIFSuccessResponse],
+        success_status_code=status.HTTP_200_OK
+    )
+)
+async def view_check_exist_cif(
+        request_body: CheckExistCIFRequest,
+        current_user=Depends(get_current_user_from_header())
+):
+    check_exist_data = await CtrCustomer(current_user).ctr_check_exist_cif(cif_number=request_body.cif_number)
+    return ResponseData[CheckExistCIFSuccessResponse](**check_exist_data)
