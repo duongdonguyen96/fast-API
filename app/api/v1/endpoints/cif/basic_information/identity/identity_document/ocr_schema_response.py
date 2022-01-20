02b991ca488b8b7e39b532b1563bb30acbff7b33
aitcq <aitcq@scb.com.vn>
2022-01-20 10:11:04 +0700

    [refactor](#29): bypass min_length

--- app/api/v1/endpoints/cif/basic_information/identity/identity_document/ocr_schema_response.py
+++ app/api/v1/endpoints/cif/basic_information/identity/identity_document/ocr_schema_response.py
@@ -12,9 +12,9 @@ from app.api.v1.schemas.utils import DropdownResponse
 
 # I. Thông tin mặt trước CMND, CCCD
 class FrontSideIdentityCitizenCardResponse(BaseSchema):
-    identity_image_url: str = Field(..., min_length=1, description="URL hình ảnh mặt trước CMND/CCCD")
+    identity_image_url: str = Field(..., description="URL hình ảnh mặt trước CMND/CCCD")
     identity_avatar_image_uuid: Optional[str] = Field(
-        ..., min_length=1, description="Hình ảnh chân dung trên GTĐD dùng để so sánh với khuôn mặt"
+        ..., description="Hình ảnh chân dung trên GTĐD dùng để so sánh với khuôn mặt"
     )
 
 
@@ -22,7 +22,7 @@ class AddressResponse(BaseSchema):
     province: Optional[DropdownResponse] = Field(..., description="Tỉnh/Thành phố", nullable=True)
     district: Optional[DropdownResponse] = Field(..., description="Quận/Huyện", nullable=True)
     ward: Optional[DropdownResponse] = Field(..., description="Phường/Xã", nullable=True)
-    number_and_street: Optional[str] = Field(..., min_length=1, description="Số nhà, tên đường", nullable=True)
+    number_and_street: Optional[str] = Field(..., description="Số nhà, tên đường", nullable=True)
 
 
 # III. Phân tích OCR -> 3. Thông tin địa chỉ CMND, CCCD
@@ -36,13 +36,13 @@ class OCRAddressIdentityCitizenCardResponse(BaseSchema):
 ########################################################################################################################
 # III. Phân tích OCR -> 1. Giấy tờ định danh (CMND)
 class OCRFrontSideDocumentIdentityCardResponse(BaseSchema):
-    identity_number: Optional[str] = Field(..., min_length=1, description="Số GTĐD", nullable=True)
+    identity_number: Optional[str] = Field(..., description="Số GTĐD", nullable=True)
     expired_date: Optional[date] = Field(..., description="Có giá trị đến", nullable=True)
 
 
 # III. Phân tích OCR -> 2. Thông tin cơ bản (CMND)
 class OCRFrontSideBasicInfoIdentityCardResponse(BaseSchema):
-    full_name_vn: Optional[str] = Field(..., min_length=1, description="Họ và tên", nullable=True)
+    full_name_vn: Optional[str] = Field(..., description="Họ và tên", nullable=True)
     date_of_birth: Optional[date] = Field(..., description="Ngày sinh", nullable=True)
     nationality: DropdownResponse = Field(..., description="Quốc tịch")
     province: Optional[DropdownResponse] = Field(..., description="Quê quán", nullable=True)
@@ -63,7 +63,7 @@ class OCRFrontSideIdentityCardResponse(BaseSchema):
 
 
 class BackSideIdentityCardResponse(BaseSchema):
-    identity_image_url: Optional[str] = Field(..., min_length=1, description="URL hình ảnh mặt sau CMND", nullable=True)
+    identity_image_url: Optional[str] = Field(..., description="URL hình ảnh mặt sau CMND", nullable=True)
 
 
 class OCRBackSideIdentityDocumentIdentityCardResponse(BaseSchema):
@@ -74,7 +74,7 @@ class OCRBackSideIdentityDocumentIdentityCardResponse(BaseSchema):
 class OCRBackSideBasicInformationIdentityCardResponse(BaseSchema):
     ethnic: Optional[DropdownResponse] = Field(..., description="Dân tộc", nullable=True)
     religion: Optional[DropdownResponse] = Field(..., description="Tôn giáo", nullable=True)
-    identity_characteristic: Optional[str] = Field(..., min_length=1, description="Đặc điểm nhận dạng", nullable=True)
+    identity_characteristic: Optional[str] = Field(..., description="Đặc điểm nhận dạng", nullable=True)
 
 
 class OCRResultBackSideIdentityCardResponse(BaseSchema):
@@ -93,15 +93,15 @@ class OCRBackSideIdentityCardResponse(BaseSchema):
 
 # I. Thông tin Hộ chiếu
 class InformationPassportResponse(BaseSchema):
-    identity_image_url: str = Field(..., min_length=1, description="URL hình ảnh Hộ chiếu")
+    identity_image_url: str = Field(..., description="URL hình ảnh Hộ chiếu")
     identity_avatar_image_uuid: Optional[str] = Field(
-        ..., min_length=1, description="Hình ảnh chân dung trên GTĐD dùng để so sánh với khuôn mặt"
+        ..., description="Hình ảnh chân dung trên GTĐD dùng để so sánh với khuôn mặt"
     )
 
 
 # II. Phân tích OCR -> 1. Giấy tờ định danh (Hộ Chiếu)
 class OCRDocumentPassportResponse(BaseSchema):
-    identity_number: Optional[str] = Field(..., min_length=1, description="Số GTĐD", nullable=True)
+    identity_number: Optional[str] = Field(..., description="Số GTĐD", nullable=True)
     issued_date: Optional[date] = Field(..., description="Ngày cấp", nullable=True)
     place_of_issue: Optional[DropdownResponse] = Field(..., description="Nơi cấp", nullable=True)
     expired_date: Optional[date] = Field(..., description="Có giá trị đến", nullable=True)
@@ -111,13 +111,13 @@ class OCRDocumentPassportResponse(BaseSchema):
 
 # II. Phân tích OCR -> 2. Thông tin cơ bản (Hộ Chiếu)
 class OCRBasicInfoPassportResponse(BaseSchema):
-    full_name_vn: Optional[str] = Field(..., min_length=1, description="Họ và tên", nullable=True)
+    full_name_vn: Optional[str] = Field(..., description="Họ và tên", nullable=True)
     gender: Optional[DropdownResponse] = Field(..., description="Giới tính", nullable=True)
     date_of_birth: Optional[date] = Field(..., description="Ngày sinh", nullable=True)
     nationality: Optional[DropdownResponse] = Field(..., description="Quốc tịch", nullable=True)
     place_of_birth: Optional[DropdownResponse] = Field(..., description="Nơi sinh", nullable=True)
-    identity_card_number: Optional[str] = Field(..., min_length=1, description="Số CMND", nullable=True)
-    mrz_content: Optional[str] = Field(None, min_length=1, description="Mã MRZ", nullable=True)
+    identity_card_number: Optional[str] = Field(..., description="Số CMND", nullable=True)
+    mrz_content: Optional[str] = Field(None, description="Mã MRZ", nullable=True)
 
 
 # II. Phân tích OCR (HC)
@@ -162,17 +162,17 @@ class OCRFrontSideCitizenCardResponse(BaseSchema):
 
 
 class BackSideCitizenCardResponse(BaseSchema):
-    identity_image_url: Optional[str] = Field(..., min_length=1, description="URL hình ảnh mặt sau CMND", nullable=True)
+    identity_image_url: Optional[str] = Field(..., description="URL hình ảnh mặt sau CMND", nullable=True)
 
 
 class OCRBackSideIdentityDocumentCitizenCardResponse(BaseSchema):
     issued_date: Optional[date] = Field(..., description="Ngày cấp", nullable=True)
     place_of_issue: Optional[DropdownResponse] = Field(..., description="Nơi cấp", nullable=True)
-    mrz_content: Optional[str] = Field(..., min_length=1, description="Đặc điểm nhận dạng", nullable=True)
+    mrz_content: Optional[str] = Field(..., description="Đặc điểm nhận dạng", nullable=True)
 
 
 class OCRBackSideBasicInformationCitizenCardResponse(BaseSchema):
-    identity_characteristic: Optional[str] = Field(..., min_length=1, description="Đặc điểm nhận dạng", nullable=True)
+    identity_characteristic: Optional[str] = Field(..., description="Đặc điểm nhận dạng", nullable=True)
 
 
 class OCRResultBackSideCitizenCardResponse(BaseSchema):
