dcc382e3849b24ff69a14e0f6a84dd1657c7919e
aitcq <aitcq@scb.com.vn>
2022-01-20 14:52:30 +0700

    [add](#55): [API][Config] SCB Branch

--- app/api/v1/endpoints/config/branch/controller.py
+++ app/api/v1/endpoints/config/branch/controller.py
@@ -1,12 +1,25 @@
 from typing import Optional
 
 from app.api.base.controller import BaseController
-from app.api.v1.endpoints.config.branch.repository import repos_get_branch
+from app.api.v1.endpoints.repository import repos_get_data_model_config
+from app.third_parties.oracle.models.master_data.others import Branch
 
 
 class CtrBranch(BaseController):
-    async def ctr_branch_info(self, region_id: Optional[str]):
-        branches_info = self.call_repos(
-            await repos_get_branch(session=self.oracle_session, region_id=region_id)
+    async def ctr_branch_info(
+            self, region_id: Optional[str],
+            province_id: Optional[str],
+            district_id: Optional[str],
+            ward_id: Optional[str]
+    ):
+        branch_info = self.call_repos(
+            await repos_get_data_model_config(
+                region_id=region_id,
+                province_id=province_id,
+                district_id=district_id,
+                ward_id=ward_id,
+                session=self.oracle_session,
+                model=Branch
+            )
         )
-        return self.response(branches_info)
+        return self.response(branch_info)
--- app/api/v1/endpoints/config/branch/view.py
+++ app/api/v1/endpoints/config/branch/view.py
@@ -7,7 +7,7 @@ from app.api.base.schema import ResponseData
 from app.api.base.swagger import swagger_response
 from app.api.v1.dependencies.authenticate import get_current_user_from_header
 from app.api.v1.endpoints.config.branch.controller import CtrBranch
-from app.api.v1.endpoints.config.branch.schema import BranchDropdownResponse
+from app.api.v1.schemas.utils import DropdownResponse
 
 router = APIRouter()
 
@@ -17,13 +17,21 @@ router = APIRouter()
     name="SCB Branch",
     description="Lấy dữ liệu chi nhánh SCB",
     responses=swagger_response(
-        response_model=ResponseData[List[BranchDropdownResponse]],
+        response_model=ResponseData[List[DropdownResponse]],
         success_status_code=status.HTTP_200_OK
     )
 )
 async def view_branch_info(
         current_user=Depends(get_current_user_from_header()),
-        region_id: Optional[str] = Query(None, description="`id khu vực`")
+        region_id: Optional[str] = Query(None, description="`ID khu vực`"),
+        province_id: Optional[str] = Query(None, description="`ID Tỉnh`"),
+        district_id: Optional[str] = Query(None, description="`ID Quận/Huyện`"),
+        ward_id: Optional[str] = Query(None, description="`ID Phường/Xã`"),
 ):
-    branch_info = await CtrBranch(current_user).ctr_branch_info(region_id=region_id)
-    return ResponseData[List[BranchDropdownResponse]](**branch_info)
+    branch_info = await CtrBranch(current_user).ctr_branch_info(
+        region_id=region_id,
+        province_id=province_id,
+        district_id=district_id,
+        ward_id=ward_id
+    )
+    return ResponseData[List](**branch_info)
