2ea696b308532b397cd2c8d2321f97024ea6fea6
aitcq <aitcq@scb.com.vn>
2022-02-14 10:59:32 +0700

    [fix](#55): [API][Config] Account Structure Type => level, parent_id

--- app/api/v1/endpoints/config/payment_account/detail/view.py
+++ app/api/v1/endpoints/config/payment_account/detail/view.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Body, Depends
 from starlette import status
 
 from app.api.base.schema import ResponseData
@@ -8,6 +8,9 @@ from app.api.base.swagger import swagger_response
 from app.api.v1.dependencies.authenticate import get_current_user_from_header
 from app.api.v1.endpoints.config.payment_account.detail.controller import (
     CtrConfigPaymentDetail
+)
+from app.api.v1.endpoints.config.payment_account.detail.schema import (
+    AccountStructureTypeRequest
 )
 from app.api.v1.schemas.utils import DropdownResponse
 
@@ -24,8 +27,11 @@ router = APIRouter()
     )
 )
 async def view_account_structure_type_info(
-        level: int = Query(..., description="Cấp độ kiểu kiến trúc tài khoản", ge=1, le=3),
+        request_body: AccountStructureTypeRequest = Body(...),
         current_user=Depends(get_current_user_from_header())
 ):
-    account_structure_type_infos = await CtrConfigPaymentDetail(current_user).ctr_account_structure_type_info(level=level)
+    account_structure_type_infos = await CtrConfigPaymentDetail(current_user).ctr_account_structure_type_info(
+        level=request_body.level,
+        parent_id=request_body.parent_id
+    )
     return ResponseData[List[DropdownResponse]](**account_structure_type_infos)
