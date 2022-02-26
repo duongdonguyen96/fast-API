54861f58c856973259861775e00b6c51dcf363d5
cuonglv4 <cuonglv4@scb.com.vn>
2022-02-26 11:16:21 +0700

    [add](#89): call ekyc history_post_check

--- app/third_parties/services/ekyc.py
+++ app/third_parties/services/ekyc.py
@@ -258,6 +258,29 @@ class ServiceEKYC:
             logger.error(str(ex))
             return False, {"message": str(ex)}
 
+    async def get_history_post_check(self, postcheck_uuid: str):
+        api_url = f"{self.url}/api/v1/customer-service/crm/postcontrolhistory/"
+
+        query = {
+            'customer_id': postcheck_uuid
+        }
+        try:
+            async with self.session.get(url=api_url, headers=self.headers, params=query, proxy=self.proxy) as response:
+                logger.log("SERVICE", f"[CARD] {response.status} : {api_url}")
+                if response.status == status.HTTP_200_OK:
+                    return True, await response.json()
+                elif response.status == status.HTTP_400_BAD_REQUEST:
+                    return False, await response.json()
+                else:
+                    return False, {
+                        "message": ERROR_CALL_SERVICE_EKYC,
+                        "detail": "STATUS " + str(response.status)
+                    }
+
+        except Exception as ex:
+            logger.error(str(ex))
+            return False, {"message": str(ex)}
+
     async def update_post_check(self, request_data):
         api_url = f"{self.url}/api/v1/customer-service/crm/postcontrol/"
 
