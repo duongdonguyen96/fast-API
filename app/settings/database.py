712d948ef76e2b9e33cd8dff8a59594c95357843
duongdonguyen96 <67623558+duongdonguyen96@users.noreply.github.com>
2024-08-06 17:56:36 +0700

    build docker

--- app/settings/database.py
+++ app/settings/database.py
@@ -2,10 +2,10 @@ import os
 
 DB_CONFIG = {
     "host": os.getenv("HOST"),
-    "port": os.getenv("PORT"),
+    "port": os.getenv("PORT", 3306),
     "user_name": os.getenv("USER_NAME"),
     "password": os.getenv("PASSWORD"),
     "service_name": os.getenv("SERVICE_NAME"),
-    'pool_size': int(os.getenv("POOL_SIZE")),
-    'pool_recycle': int(os.getenv("POOL_RECYCLE"))
+    "pool_size": int(os.getenv("POOL_SIZE", 30)),
+    "pool_recycle": int(os.getenv("POOL_RECYCLE", 3600))
 }
